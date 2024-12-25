2dbb3a71c312e479942c28d5064e37bcbdcdc706
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-25 22:46:48 +0300

    Onion arch. altyapısı oluşturuldu. Dummy veri ile ilk controller denendi. #3

---
+++ aspnet-core-api/Infrastructure/Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Concretes;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
