24f47c2fb30ece874c95ee19656aba7d409a34bb
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-27 16:10:29 +0300

    Fresh migration.

--- aspnet-core-api/Core/Domain/Entities/Order.cs
+++ aspnet-core-api/Core/Domain/Entities/Order.cs
@@ -11,7 +11,7 @@ namespace Domain.Entities
     {
         public string Description { get; set; }
         public string Address { get; set; }
-        public int CustomerId { get; set; }
+        public Guid CustomerId { get; set; }
 
         public ICollection<Product> Products { get; set; }
         public Customer Customer { get; set; }
